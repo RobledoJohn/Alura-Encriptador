3dacac132b3f491fbeb43b40b663a2bf5dbc1c34
RobledoJohn <robledoJohn@hotmail.com>
2023-01-14 02:09:43 -0500

    encriptador funcionando

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
-
 function encriptar(){
     //obtener texto del html
     var encriptarTexto = document.getElementById("texto").value;
-    console.log(encriptarTexto);
+    //console.log(encriptarTexto);
 
     //reemplzar letras
     encriptandoa = encriptarTexto.replace(/a/g,"ai");
@@ -12,13 +10,15 @@ function encriptar(){
     encriptandoo = encriptandoi.replace(/o/g,"ober");
     encriptado = encriptandoo.replace(/u/g,"ufat");
 
+    var nuevoTexto = document.getElementById("laterales");
+    nuevoTexto.innerHTML = encriptado ;
+
     console.log(encriptado);
-    alert("encriptarTexto");
 }
 
 function desencriptar(){
     var desencriptarTexto = document.getElementById("texto").value;
-    console.log(desencriptarTexto);
+    //console.log(desencriptarTexto);
 
     //reemplzar letras
     desencriptandou = desencriptarTexto.replace(/ufat/g,"u");
@@ -27,22 +27,14 @@ function desencriptar(){
     desencriptandoe = desencriptandoi.replace(/enter/g,"e");
     desencriptado = desencriptandoe.replace(/ai/g,"a");
 
+    var nuevoTexto = document.getElementById("laterales");
+    nuevoTexto.innerHTML = desencriptado ;
+
     console.log(desencriptado);
-    alert("encriptarTexto");
 }
 
-
-/*
-    "e": "enter",
-    "i": "imes",
-    "a": "ai",
-    "o": "ober",
-    "u": "ufat"
-
-
-    encriptado = encriptarTexto.replace(/a/g,"ai");
-    encriptado = encriptarTexto.replace(/e/g,"enter");
-    encriptado = encriptarTexto.replace(/i/g,"imes");
-    encriptado = encriptarTexto.replace(/o/g,"ober");
-    encriptado = encriptarTexto.replace(/u/g,"ufat");
-*/
+function copiar(){
+    var copiarTexto = document.getElementById("laterales").textContent;
+    var newText = document.getElementById("texto");
+    newText.innerHTML = copiarTexto;
+}
